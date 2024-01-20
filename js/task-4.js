bda8850a82778dd24001e70ef1dba44d55dc0b1d
katerynapikula <149505930+katerynapikula@users.noreply.github.com>
2024-01-20 00:34:36 +0000

    Update task-4js

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,23 @@
 function getShippingCost(country) {
-switch (country) {
-case `China`:
-return ('100 кредитів');
+    const normalizedCountry = country.toLowerCase();
+    let price;
+switch (normalizedCountry) {
+case `china`:
+price = 100;
 break;
-case `Chile`:
-return ('250 кредитів');
+case `chile`:
+price = 250;
 break;
-case `Australia`:
-return ('170 кредитів');
+case `australia`:
+price = 170;
 break;
-case `Jamaica`:
-return ('120 кредитів');
+case `jamaica`:
+price = 120;
 break;
 default:
-return ("Sorry, there is no delivery to your country");
+return `Sorry, there is no delivery to your country`;
     }
+    return `Shipping to ${country} will cost ${price} credits`;
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
@@ -23,3 +26,4 @@ console.log(getShippingCost("China")); // "Shipping to China will cost 100 credi
 console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
 console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
 console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
